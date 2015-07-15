1c59b4325a08c6d541ca4f93a7a1e03db9d04083
joelworsham <joelworsham@gmail.com>
2015-07-15 10:24:24 -0400

    v1.0.0

--- applegate-faq.php
+++ applegate-faq.php
@@ -4,7 +4,7 @@
  * Description: Creates the "FAQ" custom post type.
  * Author: Joel Worsham
  * Author URI: http://realbigmarketing.com
- * Version: 0.1.0
+ * Version: 1.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Define plugin constants
-define( 'AGFAQ_VERSION', '0.1.0' );
+define( 'AGFAQ_VERSION', '1.0.0' );
 define( 'AGFAQ_DIR', plugin_dir_path( __FILE__ ) );
 define( 'AGFAQ_URL', plugins_url( '', __FILE__ ) );
 
